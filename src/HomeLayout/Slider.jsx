2a1a0ebed3af7fbc7dfca9a4b6ca683446908f63
unknown <ahamedraju152@gmail.com>
2025-08-26 21:50:17 +0600

    added a slider using swiper slider

--- src/HomeLayout/Slider.jsx
+++ src/HomeLayout/Slider.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
-import slid_img from '../assets/landing_page.jpg'
 import slid_img_2 from '../assets/banner_img2.jpg'
-import slid_img_3 from '../assets/banner_img.jpg'
+import slid_img_3 from '../assets/landing_page.jpg'
 import slider_img from '../assets/banner-img_3.jpg'
+// import Swiper from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+ 
+
 
 const Slider = () => {
     return (
-        <div>
+        <>
+        <div className='w-full'>
+        <Swiper
+          modules={[Navigation, Pagination, Autoplay]}
+          spaceBetween={30}
+          slidesPerView={1}
+          
+          pagination={{clickable: true}}
+          autoplay ={{delay: 3000, disableOnInteraction: false}}
+          loop={true}
+        className= "w-full h-[400px] md:h-[500px] lg:h-[600px]">
+       
+        <SwiperSlide>
+            <img src={slider_img} alt="slide_1" className='w-full h-full object-cover rounded-lg' />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slid_img_2} alt="slide_2" className='w-full h-full object-cover' />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slid_img_3} alt="slide_3" className='w-full h-full object-cover' />
+        </SwiperSlide>
+         <div className="swiper-button-prev !w-8 !h-8 text-white after:!text-sm"></div>
+        <div className="swiper-button-next !w-8 !h-8 text-white after:!text-sm"></div>
+        </Swiper>
+        {/* daisy ui from */}
+        </div>
+        {/* <div>
             <div className="carousel w-full">
                 <div id="slide1" className="carousel-item relative w-full">
                     <img
@@ -37,7 +71,8 @@ const Slider = () => {
                 </div>
                
             </div>
-        </div>
+        </div> */}
+        </>
     );
 };
 
